1b166ba9b96055f32b683b5fc753429855ab4d0c
Matteo Gassend <matteo.gassend@epitech.eu>
2022-03-10 09:52:13 +0100

    [add] see more button

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import MovieCard from '../components/movie/MovieCard'
 import client from '../utils/tmdb'
@@ -18,19 +19,27 @@ export default function Home() {
 
 
   useEffect(() => {
-    getMovies()
-  }, [])
+    if (movies.length === 0) {
+      getMovies()
+    }
+  }, [movies])
 
 
   return (
     <div className='w-full flex flex-col'>
       <div className='flex flex-col'>
-        <h4 className='self-center font-semibold text-xl mb-4'>Latest Movies</h4>
+        <div className='flex justify-between'>
+        <h4 className='font-semibold text-xl mb-1'>Latest Movies</h4>
+        <Link href="/movie/popular">See More</Link>
+        </div>
         <div className='gap-4 flex overflow-x-auto'>
           {
             movies.map((e) => <MovieCard key={e.id} movie={e} />)
           }
         </div>
+      </div>
+      <div className='flex flex-col items-center justify-center'>
+        <h1>Something else here...</h1>
       </div>
     </div>
   )
