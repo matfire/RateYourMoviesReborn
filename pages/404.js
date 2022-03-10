3ee307036876d3938787bb73b2a19c0b2a84db53
Matteo Gassend <matteo.gassend@epitech.eu>
2022-03-10 10:40:57 +0100

    [wip] custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <span className="font-bold text-3xl">This is not the page you&apos;re looking for</span>
+    </div>
+  )
+}
