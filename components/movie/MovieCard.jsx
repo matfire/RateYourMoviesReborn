e34e8cb39615134aecf96354e3e2f420ba25ff9a
Matteo Gassend <matteo.gassend@epitech.eu>
2022-03-10 10:04:16 +0100

    [add] click on card redirects to movie details

--- components/movie/MovieCard.jsx
+++ components/movie/MovieCard.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import client from "../../utils/tmdb";
 
 export default function MovieCard({ movie }) {
+  const router = useRouter()
+
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <div className="w-80 lg:w-96 relative group cursor-pointer ">
+      <div className="w-80 lg:w-96 relative group cursor-pointer " onClick={() => router.push(`/movie/${movie.id}`)}>
         <Image src={client.getImageUrl(movie.poster_path, "w500")} width={500} height={700} alt={movie.title} />
         <div className="transition-opacity duration-150 hidden opacity-0 bg-base-100/70 group-hover:opacity-100 lg:flex w-full h-full absolute top-0 right-0 z-10 flex-col justify-center items-center">
           <span className="font-medium text-xl text-center">{movie.title}</span>
