974fcdaf69183049c9a3a9470e8643f9fd63c62b
Matteo Gassend <mgassend@gmail.com>
2022-11-02 10:53:56 +0100

    [fix] hydration issue

--- app/movie/[id]/ImagesTab.tsx
+++ app/movie/[id]/ImagesTab.tsx
@@ -3,11 +3,19 @@ import { Tab } from '@headlessui/react'
 import Image from "next/image"
 import { Movie } from "@matfire/the_movie_wrapper/dist/types/movie"
 import client from "../../../utils/tmdb";
+import { useEffect, useState } from 'react';
 
 export default function ImagesTab({ movie }: { movie: Movie }) {
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+	if (!mounted) return null
+
 	return (
 		<Tab.Group>
-			<Tab.List className="flex justify-center tabs">
+			<Tab.List className="flex justify-center tabs mb-5">
 				<Tab>
 					{({ selected }) => (
 						<button className={selected ? "tab tab-bordered tab-active" : "tab tab-bordered"}>
@@ -18,35 +26,35 @@ export default function ImagesTab({ movie }: { movie: Movie }) {
 				<Tab>
 					{({ selected }) => (
 						<button className={selected ? "tab tab-bordered tab-active" : "tab tab-bordered"}>
-						Backdrops
-					</button>
-				)}
+							Backdrops
+						</button>
+					)}
 				</Tab>
 				<Tab>
 					{({ selected }) => (
 						<button className={selected ? "tab tab-bordered tab-active" : "tab tab-bordered"}>
-						Logos
-					</button>
-				)}
+							Logos
+						</button>
+					)}
 				</Tab>
 			</Tab.List>
 			<Tab.Panels>
 				<Tab.Panel>
 					<div className="flex overflow-x-auto snap-x gap-4 lg:snap-none">
 						{movie.images.posters.map((poster) => (
-							<Image key={poster.file_path} className="snap-center" alt={`poster ${poster.file_path}`} src={client.getImageUrl(poster.file_path, "w500")} width={500} height={720} />
+							<Image key={poster.file_path} className="snap-center w-80 h-auto object-cover" alt={`poster ${poster.file_path}`} src={client.getImageUrl(poster.file_path, "w500")} width={500} height={720} />
 						))}
 					</div>
 				</Tab.Panel>
 				<Tab.Panel>
-				<div className="flex overflow-x-auto snap-x gap-4 lg:snap-none">
+					<div className="flex overflow-x-auto snap-x gap-4 lg:snap-none">
 						{movie.images.backdrops.map((poster) => (
 							<Image key={poster.file_path} className="snap-center" alt={`poster ${poster.file_path}`} src={client.getImageUrl(poster.file_path, "w500")} width={500} height={282} />
 						))}
 					</div>
 				</Tab.Panel>
 				<Tab.Panel>
-				<div className="flex overflow-x-auto snap-x gap-4 lg:snap-none">
+					<div className="flex overflow-x-auto snap-x gap-4 lg:snap-none">
 						{movie.images.logos.map((poster) => (
 							<Image key={poster.file_path} className="snap-center" alt={`poster ${poster.file_path}`} src={client.getImageUrl(poster.file_path, "w500")} width={500} height={282} />
 						))}
